96008d8b49c335cfddf1a4f7f8f2b4911e507227
Alejandro Carvajal <alejo.carvajal03@gmail.com>
2017-01-23 18:05:21 +0000

    diseño formularios

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,20 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
         <title>Laravel</title>
     </head>
     <body>
-        Hola desde Create Form
-        <form method='POST' action="{{route ('admin.store')}}">
-            <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <label>Nombre:</label>
-            <input type="text" name="name">
-            <label>Correo Electronico:</label>
-            <input type="text" name="email">
-            <label>Password:</label>
-            <input type="password" name="password">
-            <input type="submit" name="enviar" value="Enviar Datos">
-        </form>
+        <div class="container">
+            <h2 class="text-center text-uppercase">Formulario de registro de usuarios</h2>
+            <form method='POST' action="{{route ('admin.store')}}" class="form-group">
+                <input type="hidden" name="_token" value="{{csrf_token()}}" class="form-content">
+                <label>Nombre:</label>
+                <input type="text" name="name" class="form-control">
+                <label>Correo Electronico:</label>
+                <input type="text" name="email" class="form-control">
+                <label>Password:</label>
+                <input type="password" name="password" class="form-control">
+                <input type="submit" name="enviar" value="Enviar Datos" class="btn btn-primary">
+            </form>
+        </div>
     </body>
 </html>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,21 +2,24 @@
 <html>
 <head>
 	<title>Index Admin</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<h1>Estos son los usuarios registrados</h1>
-	<a href="/admin/create">Agregar usuario</a>
-	<table border=2>
-		<th>nombre</th>
-		<th>Email</th>
-		<th>Opciones</th>
-		@foreach($usuarios as $usuario)
-			<tr>
-				<td>{{$usuario->name}}</td>
-				<td>{{$usuario->email}}</td>
-				<td><a href="">Editar </a><a href="">Eliminar</a></td>
-			</tr>
-		@endforeach
-	</table>
+	<div class="container">
+		<h1 class="text-center text-uppercase">Estos son los usuarios registrados</h1>
+		<table class="table table-hover">
+			<th>nombre</th>
+			<th>Email</th>
+			<th>Opciones</th>
+			@foreach($usuarios as $usuario)
+				<tr>
+					<td>{{$usuario->name}}</td>
+					<td>{{$usuario->email}}</td>
+					<td><a href="" class="btn btn-warning">Editar </a><a href="" class="btn btn-danger">Eliminar</a></td>
+				</tr>
+			@endforeach
+		</table>
+		<a href="/admin/create" class="btn btn-info">Agregar usuario</a>
+	</div>
 </body>
 </html>
